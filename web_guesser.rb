8e363295ba681708deae98b81499c3e1effe6238
Christopher Dale <epiphone2903@hotmail.com>
2017-03-05 15:40:43 -0700

    I3 with colors working.  Tried to do cheat code, got errors that I couldn't fix. At least at this moment.  Going to try to re-write this soon, I want to be able to see my number that I last guessed.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,62 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
+class WebGuesser
+  attr_reader :number, :response
 
-number =  rand(100)
+  def initialize
+    @number = rand(100)
+  end
+
+  RESPONSE = {
+    too_high: "too high",
+    way_too_high: "way too high",
+    too_low: "too low",
+    way_too_low: "way too low",
+    correct: "You got it right! The number is #{@number}",
+    waiting: "Please enter a guess"
+  }
+
+  BGCOLORS = {
+  way_too_high: "#d10000",
+  too_high: "#f7d4d4",
+  way_too_low: "#d10000",
+  too_low: "#f7d4d4",
+  correct: "#015b0f",
+  }
+
+
+  def build_response(guess)
+    return RESPONSE[:way_too_high] if (guess - number ) > 5
+    return RESPONSE[:too_high] if guess > number
+    return RESPONSE[:way_too_low] if (guess - number ) < -5
+    return RESPONSE[:too_low] if guess < number
+    return RESPONSE[:correct] if guess == number
+  end
+
+  def background_color(guess)
+    return BGCOLORS[:way_too_high] if (guess - number ) > 5
+    return BGCOLORS[:too_high] if guess > number
+    return BGCOLORS[:way_too_low] if (guess - number ) < -5
+    return BGCOLORS[:too_low] if guess < number
+    return BGCOLORS[:correct] if guess == number
+  end
+end
+
+  # def activate_cheat(cheat)
+  #    if cheat == "true"
+  #     return "cheat code is active: the correct answer is #{@number}"
+  #   end
+  # end
+
+web_guesser = WebGuesser.new
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  # cheat_code = params['cheat_code']
+  erb :index, :locals => {
+        :message => web_guesser.build_response(guess),
+        :background => web_guesser.background_color(guess),
+        # :cheat_code => web_guesser.activate_cheat(cheat_code)
+      }
 end
